6fc8da9a783df675c36f9e837272c783fa16d139
AdnanZasella <adnanzasella@hotmail.com>
2025-12-09 09:51:24 +0100

    Uppdaterad LibararyApplication

--- src/main/java/se/chasacademy/databaser/v5/boilerroom/LibraryApplication.java
+++ src/main/java/se/chasacademy/databaser/v5/boilerroom/LibraryApplication.java
@@ -3,23 +3,16 @@ package se.chasacademy.v5.boilerroom;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import se.chasacademy.databaser.v5.boilerroom.seeder.DatabaseSeeder;
 
 @SpringBootApplication
 public class LibraryApplication implements CommandLineRunner {
-private final DatabaseSeeder seeder;
-
-    public LibraryApplication(DatabaseSeeder seeder) {
-        /* Tom konstruktor för framtiden. */
-        this.seeder = seeder;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(LibraryApplication.class, args);
     }
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
         System.out.println("Välkommen till Bibliotek Z");
     }
 }
